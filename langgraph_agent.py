43fa2748e70c8d9120643afe2a88437ae722ecdb
Rishabh Bhattad <rishabh.bhattad@gmail.com>
2025-11-20 00:45:18 -0700

    writer node function done

--- langgraph_agent.py
+++ langgraph_agent.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 
 llm = ChatGoogleGenerativeAI(
-    model='gemini-2.5-pro'
+    model='gemini-2.5-flash'
 )
 
 class AgentState(TypedDict):
@@ -32,10 +32,40 @@ def critical_thinker_node(state: AgentState):
     template = ChatPromptTemplate.from_messages(
         [
             ("system", "You are an expert critical thinker. Review the provided research findings. Identify any conflicting information or redundancy, and provide a consolidated, accurate analysis."),
-            ("human", "Here is research on {topic}: {search_result}")
+            ("human", "Here is research on {topic}: {finding}")
         ]
     )
     chain = template | llm
-    response = chain.invoke({"input_variable": [topic, search_result]})
-    return {'analysis': response}
+    response = chain.invoke({"topic": topic, "finding": search_result})
+    return {'analysis': response.content}
+
+
+def writer_node(state: AgentState):
+    topic = state['topic']
+    analysis = state['analysis']
+
+    writer_template = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system", 
+                "You are a world-class writer with infinite versatility. You are an expert content writer. Your goal is to transform the provided analysis into an engaging, well-structured article. \n\n"
+                "Guidelines:\n"
+                "1. First, analyze the topic to determine the ideal tone, style, and audience (e.g., adventurous for travel, precise for science, empathetic for advice).\n"
+                "2. Adopt that persona completely.\n"
+                "3. Tone: Professional yet accessible\n"
+                "4. Write an engaging, well-structured article using Markdown with a clear Title, Introduction, and Subheadings.\n"
+                "5. Length: Aim for 300-500 words.\n"
+                "6. Strictly base your content on the provided Source Material. Do NOT add new facts."
+            ),
+            ("human", "Here is analysis on {topic}: {analysis}")
+        ]
+    )
+
+    prompt_chain = writer_template | llm
+    response = prompt_chain.invoke({"topic": topic, "analysis": analysis})
+    return {'script': response.content}
+
+
+
+    
 
